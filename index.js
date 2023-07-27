b97f915dd20325d4e5618a8d93c9986078c0965e
marinawex <sheep803@gmail.com>
2023-07-27 16:12:02 +0300

    Refactor: Replaced fetch call with inline data

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+const images = [
+  {"id":1,"title":"Miss Vegas","name":"Yunus Fulton","image":"assets/image20.jpg"},
+  {"id":2,"title":"Miss Alabama","name":"Audrey Chavez","image":"assets/image21.jpg"},
+  {"id":3,"title":"Miss New York","name":"Maria Luna","image":"assets/image22.jpg"},
+  {"id":4,"title":"Miss Australia","name":"Janet Wu","image":"assets/image23.jpg"},
+  {"id":5,"title":"Miss London","name":"Felicity Russo","image":"assets/image24.jpg"},
+  {"id":6,"title":"Miss Israel","name":"Valerie Campbell","image":"assets/image25.jpg"},
+  {"id":7,"title":"Miss Florida","name":"Archie Arias","image":"assets/image26.jpg"},
+  {"id":8,"title":"Miss Washington","name":"Aliza Johns","image":"assets/image27.jpg"},
+  {"id":9,"title":"Miss Atlanta","name":"Rafe Haynes","image":"assets/image28.jpg"},
+  {"id":10,"title":"Miss France","name":"Liyana Vargas","image":"assets/image29.jpg"},
+  {"id":11,"title":"Miss Ireland","name":"Joao Graves","image":"assets/image30.jpg"},
+  {"id":12,"title":"Miss Whales","name":"Casey Mckee","image":"assets/image31.jpg"}
+]
+
+
+
 const nextButton = document.getElementById("next-button");
 const prevButton = document.getElementById("prev-button");
 const buttonContainer = document.querySelector(".Buttons");
@@ -52,9 +69,9 @@ const setCurrentPage = (pageNum) => {
   });
 };
 
-fetch("images.json")
-  .then((response) => response.json())
-  .then((images) => {
+// fetch("images.json")
+//   .then((response) => response.json())
+//   .then((images) => {
     let cardContainer = document.querySelector(".Images");
 
     for (let i = 0; i < images.length; i++) {
@@ -87,10 +104,12 @@ fetch("images.json")
       setCurrentPage(currentPage - 1);
     });
 
+
     nextButton.addEventListener("click", () => {
       setCurrentPage(currentPage + 1);
     });
 
+   
     document.querySelectorAll(".pagination-number").forEach((button) => {
       button.addEventListener("click", () => {
         setCurrentPage(parseInt(button.textContent));
@@ -107,5 +126,5 @@ fetch("images.json")
         avatar.src = images[index].image;
       });
     });
-  })
-  .catch((error) => console.log("Error:", error));
+  // })
+  // .catch((error) => console.log("Error:", error));
